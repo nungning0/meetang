f9ff92439b0744068a4509ce3d0ff95c6d9a9bb8
ning <saovaluk.somwong10755@gmail.com>
2022-08-21 21:17:52 +0000

    test store load

--- test/test_expenses.py
+++ test/test_expenses.py
@@ -6,3 +6,10 @@ def test_arg_parsing():
     assert expense['price'] == 590
     assert expense['item'] == 'vacuum bottle'
     assert expense['category'] == '/workout/equip'
+
+
+def test_store_load_expense():
+    expense = {'category': 'workout', 'price': '590', 'item': 'vacuum bottle'}
+    expenses.store_expense(expense)
+    expenses_list = expenses.load_expense()
+    assert expense == expenses_list[0]
