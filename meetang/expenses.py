33d3a0548573ca9eff07d6a6c245e37487260dbf
ning <saovaluk.somwong10755@gmail.com>
2022-08-22 16:50:06 +0000

    type migration dict to Expense class

--- meetang/expenses.py
+++ meetang/expenses.py
@@ -1,3 +1,22 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Expense:
+    item: str
+    price: float
+    category: str
+
+
+def expense_i(inf):
+    print(f"this is {inf.item} cost {inf.price} category {inf.category}")
+
+
+v = Expense('fish', 900, 'food')
+expense_i(v)
+
+
 def is_price(word):
     try:
         float(word)
@@ -6,7 +25,7 @@ def is_price(word):
         return False
 
 
-def parse_arguments(user_input):
+def parse_arguments(user_input) -> Expense:
     item = ''
     category = ''
     price = -1
@@ -21,23 +40,23 @@ def parse_arguments(user_input):
             item = item.lstrip()
             item += " " + arg
 
-    return {'category': category, 'price': price, 'item': item}
+    return Expense(item, price, category)
 
 
-def store_expense(expense):
-    csv_line = expense['item'] + ',' + str(expense['price']) + ',' + expense['category']
+def store_expense(expense: Expense):
+    csv_line = expense.item + ',' + str(expense.price) + ',' + expense.category + '\n'
     with open('expenses.csv', 'a+') as file_object:
         file_object.write(csv_line)
 
 
-def load_expense():
-    expenses = []
+def load_expense() -> List[Expense]:
+    expenses : List[Expense] = []
 
     with open('expenses.csv') as file_object:
         lines = file_object.readlines()
         for line in lines:
-            item, price, category, *_ = line.split(',')
-            expense = {'category': category, 'price': price, 'item': item}
+            item, price, category, *_ = line.rstrip().split(',')
+            expense = Expense(item, float(price), category)
             expenses.append(expense)
 
     return expenses
@@ -49,8 +68,8 @@ if __name__ == "__main__":
         if user_input == 'q':
             break
 
-        parsed_expense = parse_arguments(user_input)
+        parsed_expense : Expense = parse_arguments(user_input)
         print(
-            parsed_expense['category'] + ' is added to category | ' + str(
-                parsed_expense['price']) + ' is added to price | ' + parsed_expense['item'] + ' is added to item')
+            f"""{parsed_expense.item} is added to item 
+                {parsed_expense.price} is added to price {parsed_expense.category} is added to category)""")
         store_expense(parsed_expense)
--- test/test_expenses.py
+++ test/test_expenses.py
@@ -1,15 +1,37 @@
+from pathlib import Path
+
+import pytest
+
 from meetang import expenses
+from meetang.expenses import Expense
+
+
+def remove_test_expenses():
+    expenses_file = Path.cwd() / "expenses.csv"
+    if expenses_file.exists():
+        expenses_file.unlink()
+
+
+@pytest.fixture(autouse=True)
+def remove_config_if_created():
+    yield
+    remove_test_expenses()
 
 
 def test_arg_parsing():
-    expense = expenses.parse_arguments('vacuum bottle /workout/equip 590')
-    assert expense['price'] == 590
-    assert expense['item'] == 'vacuum bottle'
-    assert expense['category'] == '/workout/equip'
+    expense: Expense = expenses.parse_arguments('vacuum bottle /workout/equip 590')
+    assert expense.price == 590
+    assert expense.item == 'vacuum bottle'
+    assert expense.category == '/workout/equip'
 
 
 def test_store_load_expense():
-    expense = {'category': 'workout', 'price': '590', 'item': 'vacuum bottle'}
-    expenses.store_expense(expense)
+    expense_1 = Expense('vacuum', 600, 'workout')
+    expenses.store_expense(expense_1)
     expenses_list = expenses.load_expense()
-    assert expense == expenses_list[0]
+    assert expense_1 == expenses_list[0]
+
+    expense_2 = Expense('fish', 9, 'food')
+    expenses.store_expense(expense_2)
+    expenses_list_2 = expenses.load_expense()
+    assert expense_2 == expenses_list_2[1]
